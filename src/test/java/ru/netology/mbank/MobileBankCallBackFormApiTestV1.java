70fc0ab64675dda9b2cce3faafd84072d6f3b09f
Elena <qa2301-mail.ru>
2025-02-23 00:47:37 +0300

    callBackFormTestV1: added negative tests

--- src/test/java/ru/netology/mbank/MobileBankCallBackFormApiTestV1.java
+++ src/test/java/ru/netology/mbank/MobileBankCallBackFormApiTestV1.java
@@ -13,22 +13,25 @@ import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
-public class MobileBankCallBackFormApiTestV1{
+public class MobileBankCallBackFormApiTestV1 {
 
-    //инициализация переменной объекта driver
+    //инициализация переменных
     private WebDriver driver;
+    private WebElement result;
+    private WebElement imageCart;
+    private List<WebElement> inputElements;
 
     @BeforeAll
     static void setupAll() {
         WebDriverManager.chromedriver().setup(); //настройка драйвера для хрома
     }
 
-    // загрузка драйвера с определенными параметрами.
+
     @BeforeEach
     void setup() {
+        // загрузка драйвера с определенными параметрами.
         ChromeOptions options = new ChromeOptions(); //  набор параметров для драйвера
         options.addArguments("--disable-dev-shm-usage"); // отключает использование временного хранилища
         options.addArguments("--dno-sandbox"); // отключает песочницу безопасности
@@ -44,25 +47,95 @@ public class MobileBankCallBackFormApiTestV1{
         driver = null;
     }
 
+    // проверка наличия рекламного изображения карты
+    @Test
+    public void shouldImageCard() {
+        imageCart = driver.findElement(By.cssSelector("[alt='Альфа-Карта Premium']"));// найти рекламное изображение карты
+        assertTrue(imageCart.isDisplayed()); //подтверждение наличия фотографии рекламной карты
+    }
+
     @Test
     public void shouldCreateCallbackRequest() {
 
-        WebElement imageCart = driver.findElement(By.cssSelector("[alt='Альфа-Карта Premium']"));// найти рекламное изображение карты
-        assertTrue(imageCart.isDisplayed()); //подтверждение наличия фотографии рекламной карты
-
         //Форма заявки
-        List<WebElement> inputElements = driver.findElements(By.cssSelector("[class='input__control']")); //найти список элементов
+        inputElements = driver.findElements(By.cssSelector("[class='input__control']")); //найти список элементов
         inputElements.get(0).sendKeys("Петров Иван"); //ввести в поле фамилию и имя
         inputElements.get(1).sendKeys("+71112223344"); //ввести номер телефона
 
-        driver.findElement(By.cssSelector("[data-test-id='agreement']")).click(); //поставить подтвердить согласие на обработку перс.данных
+        driver.findElement(By.cssSelector("[data-test-id='agreement']")).click(); // подтвердить согласие на обработку перс.данных
         driver.findElement(By.cssSelector("[class='button button_view_extra button_size_m button_theme_alfa-on-white']")).click(); //нажать на кнопку для отправки
 
-        WebElement result = driver.findElement(By.cssSelector("[data-test-id='order-success']"));//проверяем наличие информации о создании заявки на обратный звонок через появление инф.сообщения
+        result = driver.findElement(By.cssSelector("[data-test-id='order-success']"));//проверяем наличие информации о создании заявки на обратный звонок через появление инф.сообщения
 
-        assertTrue(result.isDisplayed());
-        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", result.getText().trim());
+        assertTrue(result.isDisplayed());  // проверяем появление сообщения
+        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", result.getText().trim()); //проверяем текст сообщения
 
+    }
+
+    // Не заполнены Фамилия и Имя
+    @Test
+    public void shouldNotCreateCallbackRequestNotInputName() {
+        inputElements = driver.findElements(By.cssSelector("[class='input__sub']")); //  создаем коллекцию с тестами ошибок для полей ввода
+
+        driver.findElement(By.cssSelector("[class='button button_view_extra button_size_m button_theme_alfa-on-white']")).click(); // нажимаем на кнопку "Продолжить"
+        result = inputElements.get(0); // сохраняем в переменной result первый элемент коллекции
+        assertTrue(result.isDisplayed()); // проверяем, что сообщение отображается
+        assertEquals("Поле обязательно для заполнения", result.getText().trim()); // проверяем текст сообщения
+
+    }
+
+    // Имя заполнено на латинице
+    @Test
+    public void shouldNotCreateCallbackRequestWithLatinName() {
+        inputElements = driver.findElements(By.cssSelector("[class='input__control']")); // создаем коллекцию полей ввода
+        inputElements.get(0).sendKeys("Petrov Ivan"); // вводим в первое поле Фамилию и Имя на латинице
+
+        inputElements = driver.findElements(By.cssSelector("[class='input__sub']")); // создаем коллекцию элементов с текстами ошибок
+
+        driver.findElement(By.cssSelector("[class='button button_view_extra button_size_m button_theme_alfa-on-white']")).click(); // нажимаем на кнопку "Продолжить"
+        result = inputElements.get(0); // сохраняем в переменной текст первого элемента коллекции
+        assertTrue(result.isDisplayed()); // проверяем, что сообщение отображается
+        assertEquals("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.", result.getText().trim()); // проверяем текст сообщения
+    }
+
+    // Не заполнен телефон
+    @Test
+    public void shouldNotCreateCallbackRequestNotPhoneNumber() {
+        inputElements = driver.findElements(By.cssSelector("[class='input__control']")); // создаем коллекцию полей ввода
+        inputElements.get(0).sendKeys("Петров Иван"); // вводим в первое поле корректные Фамилию и Имя
+
+        inputElements = driver.findElements(By.cssSelector("[class='input__sub']")); // создаем коллекцию с текстами ошибок
+
+        driver.findElement(By.cssSelector("[class='button button_view_extra button_size_m button_theme_alfa-on-white']")).click(); // нажимаем на кнопку "Продолжить"
+        result = inputElements.get(1);   // сохраняем в переменной текст второго элемента коллекции
+        assertTrue(result.isDisplayed()); // проверяем, что сообщение отображается
+        assertEquals("Поле обязательно для заполнения", result.getText().trim()); // проверяем текст сообщения
+
+    }
+
+    // Номер телефона введен с ошибкой
+    @Test
+    public void shouldNotCreateCallbackRequestIncorrectPhoneNumber() {
+        inputElements = driver.findElements(By.cssSelector("[class='input__control']")); // создаем коллекцию полей ввода
+        inputElements.get(0).sendKeys("Петров Иван"); // вводим в первое поле корректные Фамилию и Имя
+        inputElements.get(1).sendKeys("799922211333"); // вводим номер телефона с ошибкой
+
+        inputElements = driver.findElements(By.cssSelector("[class='input__sub']")); // создаем коллекцию элементов с текстами ошибок
+
+        driver.findElement(By.cssSelector("[class='button button_view_extra button_size_m button_theme_alfa-on-white']")).click(); // нажимаем на кнопку "Продолжить"
+        result = inputElements.get(1);  // сохраняем в переменной текст второго элемента коллекции
+        assertTrue(result.isDisplayed()); // проверяем, что сообщение отображается
+        assertEquals("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.", result.getText().trim()); // проверяем текст сообщения
+    }
+
+    // Не стоит согласие на обработку персональных данных
+    @Test
+    public void shouldNotCreateCallbackRequestNoConsentProcessing() {
+        inputElements = driver.findElements(By.cssSelector("[class='input__control']")); // создаем коллекцию полей ввода
+        inputElements.get(0).sendKeys("Петров Иван"); // вводим в первое поле корректные Фамилию и Имя
+        inputElements.get(1).sendKeys("+71112223344"); // вводим номер телефона
+
+        assertFalse(driver.findElement(By.cssSelector("[data-test-id='agreement']")).isSelected()); // проверяем отсутствие заполнения чек-бокса
     }
 
 }
